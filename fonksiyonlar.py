7acc3457b519ad036ea1800120bccae14a6c9023
sevkikaragol <sevkaragol@gmail.com>
2021-03-19 16:10:22 +0300

    title-anahtar kelime guncellemesi

--- fonksiyonlar.py
+++ fonksiyonlar.py
@@ -3,131 +3,139 @@ from bs4 import BeautifulSoup
 import trStopWords
 import operator
 
+# -------------------------------------------------------------------------------------------------------------------------------------
 
+
+def sembolTemizler(tumKelimeler):
+    sembolsuzKelimeler = []
+    semboller = "•!@$%^*()_{}\"<>,./;'&[’]-=·›:`~+|#?1234567890"
+    #turkceKarakter= {"Ğ":"G","ğ":"g","Ü":"U","ü":"u","Ş":"S","İ":"I","Ö":"O","ö":"o","Ç":"C","ç":"c","ş":"s"}
+
+    for kelime in tumKelimeler:
+
+        kelime = kelime.replace("İ", "I")
+        kelime = kelime.replace("ı", "i")
+        kelime = kelime.replace("Ç", "C")
+        kelime = kelime.replace("Ö", "O")
+        kelime = kelime.replace("Ğ", "G")
+        kelime = kelime.replace("Ş", "S")
+        kelime = kelime.replace("Ü", "U")
+        kelime = kelime.replace("ç", "c")
+        kelime = kelime.replace("ö", "o")
+        kelime = kelime.replace("ğ", "g")
+        kelime = kelime.replace("ş", "s")
+        kelime = kelime.replace("ü", "u")
+
+        kelime = kelime.lower()
+
+        for sembol in semboller:
+            if sembol in kelime:
+                kelime = kelime.replace(sembol, "")
+
+        if(len(kelime) > 0):
+            sembolsuzKelimeler.append(kelime)
+
+    return sembolsuzKelimeler
+
+# -------------------------------------------------------------------------------------------------------------------------------------
+
+
+def sozlukOlustur(tumKelimeler):
+    kelimeSayisi = {}
+
+    for kelime in tumKelimeler:
+        if kelime in kelimeSayisi:
+            kelimeSayisi[kelime] += 1
+        else:
+            kelimeSayisi[kelime] = 1
+
+    return kelimeSayisi
+# -------------------------------------------------------------------------------------------------------------------------------------
 
 
 def kelimeFrekans(url):
     tumKelimeler = []
 
-    def sembolTemizler(tumKelimeler):
-        sembolsuzKelimeler = []
-        semboller = "!@$%^*()_{}\"<>,./;'&[’]-=·›:`~+|#?1234567890"
-        #turkceKarakter= {"Ğ":"G","ğ":"g","Ü":"U","ü":"u","Ş":"S","İ":"I","Ö":"O","ö":"o","Ç":"C","ç":"c","ş":"s"}
-        
-        for kelime in tumKelimeler:
-            
-            kelime = kelime.replace("İ", "I")
-            kelime = kelime.replace("ı","i")
-            kelime = kelime.replace("Ç", "C")
-            kelime = kelime.replace("Ö", "O")
-            kelime = kelime.replace("Ğ", "G")
-            kelime = kelime.replace("Ş", "S")
-            kelime = kelime.replace("Ü", "U")
-            kelime = kelime.replace("ç", "c")
-            kelime = kelime.replace("ö", "o")
-            kelime = kelime.replace("ğ", "g")
-            kelime = kelime.replace("ş", "s")
-            kelime = kelime.replace("ü", "u")
-
-            kelime = kelime.lower()
-
-            for sembol in  semboller:
-                if sembol  in kelime:
-                    kelime = kelime.replace(sembol,"")
-
-            if(len(kelime) > 0):
-                sembolsuzKelimeler.append(kelime)
-
-        return sembolsuzKelimeler
-
     r = requests.get(url)
 
+    soup = BeautifulSoup(r.content, "html.parser")
 
-    soup = BeautifulSoup(r.content,"html.parser")
-    
     for kelimeGruplari in soup.find_all("body"):
         icerik = kelimeGruplari.text
         #print("************************ "+ icerik + " *********************************")
         #kelimeler = icerik.lower().split()
         kelimeler = icerik.split()
 
-
         for kelime in kelimeler:
             tumKelimeler.append(kelime)
-            #print(kelime)
+            # print(kelime)
 
     tumKelimeler = sembolTemizler(tumKelimeler)
 
-    #kelime silme esnasında sıranın kaybedilmemesi için listenin ilk halinin kopyası üzerinde geziyoruz.
+    # kelime silme esnasında sıranın kaybedilmemesi için listenin ilk halinin kopyası üzerinde geziyoruz.
     kopya = tumKelimeler.copy()
 
     for kelime in kopya:
         if(trStopWords.isStopWord(kelime)):
             tumKelimeler.remove(kelime)
 
-
-
-
-
-    def sozlukOlustur(tumKelimeler):
-        kelimeSayisi = {}
-
-        for kelime in tumKelimeler:
-            if kelime in kelimeSayisi:
-                kelimeSayisi[kelime] += 1
-            else:
-                kelimeSayisi[kelime] = 1
-
-        return kelimeSayisi
-
-
     kelimeSayisi = sozlukOlustur(tumKelimeler)
 
-    #kelimeSirali ->> frekansa göre sirali sözlük hali
-    kelimeSirali = sorted(kelimeSayisi.items(),key = operator.itemgetter(1),reverse = True)
+    # kelimeSirali ->> frekansa göre sirali sözlük hali
+    kelimeSirali = sorted(kelimeSayisi.items(),
+                          key=operator.itemgetter(1), reverse=True)
 
     return kelimeSirali
+# -------------------------------------------------------------------------------------------------------------------------------------
 
 
-def anahtarKelime(sozluk):
+def anahtarKelime(sozluk, url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+    titleString = soup.title.string.split()
+    titleString = sembolTemizler(titleString)
+
+    kopya = titleString.copy()
+
+    for kelime in kopya:
+        if(trStopWords.isStopWord(kelime)):
+            titleString.remove(kelime)
+
     sayac = 0
     anahtarSozluk = []
 
     for kelime in sozluk:
-        if (sayac==7):
+        if (sayac == 5):
             break
+        anahtarSozluk.append((kelime[0], kelime[1]))
+        sayac += 1
+    ornekList = list()
+    for i, j in anahtarSozluk:
+        ornekList.append(i)
 
-        anahtarSozluk.append((kelime[0],kelime[1]))
-        sayac+=1
+    for kelime in titleString:
+        if(kelime not in ornekList):
+            anahtarSozluk.append((kelime, int(anahtarSozluk[0][1]*1/4)))
+
     return anahtarSozluk
-        
+# -------------------------------------------------------------------------------------------------------------------------------------
 
-def benzerlikOrani(anahtarKelimeler,tumKelimeler):
-    #gelen veriler liste seklindedir. [(),(),()]
-    anahtarKelimeler = anahtarKelimeler[0:7]
-    tumKelimeler = tumKelimeler[0:7]
-    print(anahtarKelimeler)
-    print(tumKelimeler)
-    
+
+def benzerlikOrani(anahtarKelimeler, tumKelimeler):
+    # gelen veriler liste seklindedir. [(),(),()]
     sayac = 0
     kelimeSayisi = 0
     for anahtarKelime in anahtarKelimeler:
         for kelime in tumKelimeler:
             if(anahtarKelime[0] == kelime[0]):
-                sayac+=(kelime[1]+anahtarKelime[1])
-                
-            
-                
-    
+                sayac += (kelime[1]+anahtarKelime[1])
+
     for kelime in tumKelimeler:
         kelimeSayisi += kelime[1]
-    
+
     for kelime in anahtarKelimeler:
         kelimeSayisi += kelime[1]
-    
+
     return ((float(sayac)/float(kelimeSayisi))*100.0)
 
-
-
-
-
+    # -------------------------------------------------------------------------------------------------------------------------------------
--- webIndex.py
+++ webIndex.py
@@ -1,119 +1,109 @@
-from flask import Flask, render_template,request,flash
+from flask import Flask, render_template, request, flash
 import fonksiyonlar as fs
 
 
 app = Flask(__name__)
-app.secret_key="webIndex"
+app.secret_key = "webIndex"
 
-#index.html 
+# index.html
+
+
 @app.route("/")
 def index():
 
     return render_template("index.html")
-#index.html son
+# index.html son
 
 
-###########asama1 
+# asama1
 @app.route("/asama1")
 def asama1():
-    
-    return render_template("asama1.html",kontrol=False)
+
+    return render_template("asama1.html", kontrol=False)
 
 
-@app.route('/asama1',methods =['POST'])
+@app.route('/asama1', methods=['POST'])
 def getValue():
     url = request.form['text-url']
-    
+
     if (len(url) == 0):
-        flash("Lütfen URL girişi yapınız.","danger")
-        return render_template("asama1.html",kontrol=False)
-    
-    sozluk = fs.kelimeFrekans(url)    
-    return render_template("asama1.html",kontrol=True,sozluk=sozluk)
+        flash("Lütfen URL girişi yapınız.", "danger")
+        return render_template("asama1.html", kontrol=False)
+
+    sozluk = fs.kelimeFrekans(url)
+    return render_template("asama1.html", kontrol=True, sozluk=sozluk)
+
+ # asama1
 
 
- #####asama1   
-
-
-#####asama23
+# asama23
 @app.route("/asama23")
 def asama23():
-    return render_template("asama23.html",kontrol=False)
+    return render_template("asama23.html", kontrol=False)
 
 
-@app.route('/asama23',methods =['POST'])
+@app.route('/asama23', methods=['POST'])
 def getValue23():
     url1 = request.form['url1']
     url2 = request.form['url2']
 
     if (len(url1) == 0 or len(url2) == 0):
-        flash("Lütfen URL girişi yapınız.","danger")
-        return render_template("asama23.html",kontrol=False)
+        flash("Lütfen URL girişi yapınız.", "danger")
+        return render_template("asama23.html", kontrol=False)
+
+    sozluk1 = fs.anahtarKelime(fs.kelimeFrekans(url1), url1)
+    sozluk2 = fs.anahtarKelime(fs.kelimeFrekans(url2), url2)
+
+    oran = round(fs.benzerlikOrani(sozluk1, sozluk2), 2)
+
+    return render_template("asama23.html", kontrol=True, sozluk1=sozluk1, sozluk2=sozluk2, oran=oran)
 
 
-    sozluk1 = fs.anahtarKelime(fs.kelimeFrekans(url1))
-    sozluk2 = fs.anahtarKelime(fs.kelimeFrekans(url2))
-
-    oran = round(fs.benzerlikOrani(fs.kelimeFrekans(url1),fs.kelimeFrekans(url2)),2)
-
-    return render_template("asama23.html",kontrol=True,sozluk1=sozluk1,sozluk2=sozluk2,oran=oran)
+# asama23
 
 
-
-
-#####asama23
-
-
-
-###asama4
-@app.route("/asama4")
+# asama4
+@ app.route("/asama4")
 def asama4():
     return render_template("asama4.html")
 
 
-@app.route('/asama4',methods =['POST'])
+@ app.route('/asama4', methods=['POST'])
 def getValue4():
 
     url1 = request.form['url1']
     url2 = request.form['url2']
 
     if (len(url1) == 0 or len(url2) == 0):
-        flash("Lütfen URL girişi yapınız.","danger")
-        return render_template("asama4.html",kontrol=False)
-    
-   
+        flash("Lütfen URL girişi yapınız.", "danger")
+        return render_template("asama4.html", kontrol=False)
+
     return render_template("asama4.html")
 
 
+# asama4
 
 
-##asama4
+# asama5
 
-
-##asama5
-
-@app.route("/asama5")
+@ app.route("/asama5")
 def asama5():
     return render_template("asama5.html")
 
 
-@app.route('/asama5',methods =['POST'])
+@ app.route('/asama5', methods=['POST'])
 def getValue5():
 
     url1 = request.form['url1']
     url2 = request.form['url2']
 
     if (len(url1) == 0 or len(url2) == 0):
-        flash("Lütfen URL girişi yapınız.","danger")
-        return render_template("asama5.html",kontrol=False)
-    
-   
+        flash("Lütfen URL girişi yapınız.", "danger")
+        return render_template("asama5.html", kontrol=False)
+
     return render_template("asama5.html")
 
 
-
-##asama5
-
+# asama5
 if __name__ == "__main__":
     app.run(debug=True)
-
